701196582cde248a0b3b91f7495025fbcad862e9
ViktoriaArsenteva <vika.arsentievaa@yandex.ru>
2023-03-19 05:34:36 +0300

    RedBlackTree

--- Tree/Main.java
+++ Tree/Main.java
@@ -6,17 +6,6 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        // HashMap hm = new HashMap();
-        // for(int i=0; i<10; i++){
-        //     hm.add(i, i*i);
-        // }
-        // hm.add(150, 45);
-        // hm.del(5);
-        // hm.del(13);
-        // hm.print();
-
-        // System.out.println(hm.find(150));
-        // System.out.println(hm.find(143));
         final RedBlackTree tree = new RedBlackTree();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             while (true) {
@@ -31,5 +20,12 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+    // final RedBlackTree tree = new RedBlackTree();
+    //     for(int i = 0; i < 10;i++){
+    //         tree.add(i);
+    //         System.out.println("Well done");
+    //     }
+    
     }
 }
